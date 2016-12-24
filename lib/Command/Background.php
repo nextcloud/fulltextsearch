c54e48f5105a6609ea445c88b3250d09a08bbc86
daita <maxence@pontapreta.net>
2016-12-24 16:46:26 -0100

    new command

---
+++ lib/Command/Background.php
@@ -0,0 +1,58 @@
+<?php
+
+/**
+ * Nextcloud - nextant
+ *
+ * This file is licensed under the Affero General Public License version 3 or
+ * later. See the COPYING file.
+ *
+ * @author Maxence Lange <maxence@pontapreta.net>
+ * @copyright Maxence Lange 2016
+ * @license GNU AGPL version 3 or any later version
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as
+ * published by the Free Software Foundation, either version 3 of the
+ * License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Affero General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ */
+namespace OCA\Nextant\Command;
+
+use OC\Core\Command\Base;
+use Symfony\Component\Console\Input\InputArgument;
+use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Input\InputOption;
+use Symfony\Component\Console\Output\OutputInterface;
+use Symfony\Component\Console\Question\ConfirmationQuestion;
+
+class Background extends Base
+{
+
+    public function __construct($backgroundService)
+    {
+        parent::__construct();
+        $this->backgroundService = $backgroundService;
+    }
+
+    protected function configure()
+    {
+        parent::configure();
+        $this->setName('nextant:background')->setDescription('run the background index');
+    }
+
+    protected function execute(InputInterface $input, OutputInterface $output)
+    {
+        $this->backgroundService->run();       
+    }
+}
+
+
+
