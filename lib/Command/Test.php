51053204dbf8a3bc54872578652171f99a6e7d2c
Maxence Lange <maxence@pontapreta.net>
2017-02-09 00:42:47 -0100

    must set address/core in arguments

--- lib/Command/Test.php
+++ lib/Command/Test.php
@@ -27,6 +27,7 @@
 namespace OCA\Nextant\Command;
 
 use OC\Core\Command\Base;
+use OCA\Nextant\Service\SolrAdminService;
 use \OCA\Nextant\Service\TestService;
 use \OCA\Nextant\Items\ItemDocument;
 use Symfony\Component\Console\Input\InputArgument;
@@ -34,197 +35,251 @@ use Symfony\Component\Console\Input\InputInterface;
 use Symfony\Component\Console\Input\InputOption;
 use Symfony\Component\Console\Output\OutputInterface;
 
-class Test extends Base
-{
+class Test extends Base {
 
-    private $configService;
+	private $configService;
 
-    private $solrService;
+	private $solrService;
 
-    private $solrTools;
+	private $solrTools;
 
-    private $solrAdmin;
+	private $solrAdmin;
 
-    private $settingsController;
+	private $settingsController;
 
-    public function __construct($configService, $solrService, $solrTools, $solrAdmin, $indexService, $settingsController)
-    {
-        parent::__construct();
-        $this->configService = $configService;
-        $this->solrService = $solrService;
-        $this->solrTools = $solrTools;
-        $this->solrAdmin = $solrAdmin;
-        $this->indexService = $indexService;
-        $this->settingsController = $settingsController;
-    }
+	public function __construct(
+		$configService, $solrService, $solrTools, SolrAdminService $solrAdmin, $indexService,
+		$settingsController
+	) {
+		parent::__construct();
+		$this->configService = $configService;
+		$this->solrService = $solrService;
+		$this->solrTools = $solrTools;
+		$this->solrAdmin = $solrAdmin;
+		$this->indexService = $indexService;
+		$this->settingsController = $settingsController;
+	}
 
-    protected function configure()
-    {
-        parent::configure();
-        $this->setName('nextant:test')->setDescription('test your Nextant configuration');
-    }
+	protected function configure() {
+		parent::configure();
+		$this->setName('nextant:test')
+			 ->setDescription('test your Nextant configuration')
+			 ->addArgument('address', InputArgument::REQUIRED, 'address of the solr to test')
+			 ->addArgument('core', InputArgument::REQUIRED, 'core to test');
+	}
 
-    protected function execute(InputInterface $input, OutputInterface $output)
-    {
-        
-        // test ping
-        if (! $this->test_ping($output))
-            return;
-            
-            // test schema
-        if (! $this->test_schema($output))
-            return;
-            
-            // test extract
-        if (! $this->test_extract($output))
-            return;
-            
-            // test update
-        if (! $this->test_update($output))
-            return;
-            
-            // test search
-        if (! $this->test_search($output))
-            return;
-            
-            // test delete
-        if (! $this->test_delete($output))
-            return;
-        
-        $output->writeln('');
-        $output->writeln('All test results were fine');
-    }
+	protected function execute(InputInterface $input, OutputInterface $output) {
 
-    private function test_ping($output)
-    {
-        $output->write(' - Pinging Solr: ');
-        if (! $this->solrAdmin->ping($ierror)) {
-            $output->writeln('<error>fail</error> - ' . $ierror->getCode());
-            return false;
-        }
-        
-        $output->writeln('<info>ok</info>');
-        return true;
-    }
+		if (!$address = $input->getArgument('address')) {
+			$output->writeln('You need to specify the address to test');
+			return;
+		}
 
-    private function test_schema($output)
-    {
-        $output->write(' - Checking Solr schema: ');
-        if (! $this->solrAdmin->checkSchema($ierror)) {
-            $output->writeln('<error>fail</error> - ' . $ierror->getCode());
-            return false;
-        }
-        
-        $output->writeln('<info>ok</info>');
-        return true;
-    }
+		if (!$core = $input->getArgument('core')) {
+			$output->writeln('You need to specify the core');
+			return;
+		}
 
-    private function test_extract($output)
-    {
-        $output->write(' - Extracting test document: ');
-        $doc = TestService::generateTestDocument(1, '_nextant_test', __DIR__ . '/../../LICENSE', '/LICENSE');
-        $data = array(
-            $doc
-        );
-        $solrDocs = null;
-        $this->indexService->extract(ItemDocument::TYPE_TEST, '_nextant_test', $data, $solrDocs, true, $ierror);
-        
-        if (! $doc->isProcessed()) {
-            $output->writeln('<error>fail</error> - ' . $ierror->getCode());
-            return false;
-        }
-        
-        $output->writeln('<info>ok</info>');
-        return true;
-    }
+		$client = $this->solrService->getClientConfig()['endpoint']['localhost'];
+		$tmpConfig = array(
+			'solr_url'     => $address,
+			'solr_core'    => $core,
+			'solr_timeout' => 30
+		);
 
-    private function test_update($output)
-    {
-        $output->write(' - Updating test document: ');
-        $doc = TestService::generateTestDocument(1, '_nextant_test', __DIR__ . '/../../LICENSE', '/LICENSE2');
-        $asource = $this->indexService->getDocuments(ItemDocument::TYPE_TEST, '_nextant_test', 1, $ierror);
-        
-        if ($asource === false || sizeof($asource) != 1 || (! key_exists('test_1', $asource))) {
-            $output->writeln('<error>fail</error> - ' . $ierror->getCode());
-            return false;
-        }
-        
-        $source = $asource['test_1'];
-        
-        $doc->setPath('/LICENSE2');
-        $doc->setShare(array(
-            'nextant_test_share'
-        ));
-        $doc->setShareGroup(array(
-            'nextant_test_share_group'
-        ));
-        $doc->deleted(false);
-        
-        $data = array(
-            $doc
-        );
-        $this->indexService->updateDocuments(ItemDocument::TYPE_TEST, '_nextant_test', $data, $asource, $ierror);
-        
-        if (! $this->solrTools->commit(false, $ierror)) {
-            $output->writeln('<error>fail</error> - ' . $ierror->getCode());
-            return false;
-        }
-        
-        if (! $source->isUpdated()) {
-            $output->writeln('<error>fail</error> - ' . $ierror->getCode());
-            return false;
-        }
-        
-        $output->writeln('<info>ok</info>');
-        return true;
-    }
+		if (!$this->solrService->setClient($tmpConfig)) {
+			$output->writeln('Address is invalid');
 
-    private function test_search($output)
-    {
-        $output->write(' - Searching test document: ');
-        
-        $keyword = 'LICENSE';
-        $this->solrService->setOwner('_nextant_test');
-        if ($result = $this->solrService->search($keyword, array(), $ierror)) {
-            if (sizeof($result) > 0) {
-                
-                foreach ($result as $doc) {
-                    if ($doc->getType() === ItemDocument::TYPE_TEST && $doc->getId() === 1) {
-                        $output->writeln('<info>ok</info>');
-                        return true;
-                    }
-                }
-                
-                // CHECK ID DOCUMENT
-                $output->writeln('<error>fail</error>');
-                return false;
-            }
-            
-            $output->writeln('<error>fail</error>');
-            return false;
-        }
-        
-        $output->writeln('<error>fail</error> - ' . $ierror->getCode());
-        return false;
-    }
+			return;
+		}
 
-    private function test_delete($output)
-    {
-        $output->write(' - Deleting test document: ');
-        
-        $doc = new ItemDocument(ItemDocument::TYPE_TEST, 1);
-        $data = array(
-            $doc
-        );
-        $this->indexService->removeDocuments($data, $ierror);
-        if ($doc->isRemoved()) {
-            $output->writeln('<info>ok</info>');
-            return true;
-        }
-        
-        $output->writeln('<error>fail</error> - ' . $ierror->getCode());
-        return false;
-    }
+		// test ping
+		if (!$this->test_ping($output)) {
+			return;
+		}
+
+		// test schema
+		if (!$this->test_schema($output)) {
+			return;
+		}
+
+		// test extract
+		if (!$this->test_extract($output)) {
+			return;
+		}
+
+		// test update
+		if (!$this->test_update($output)) {
+			return;
+		}
+
+		// test search
+		if (!$this->test_search($output)) {
+			return;
+		}
+
+		// test delete
+		if (!$this->test_delete($output)) {
+			return;
+		}
+
+		$output->writeln('');
+		$output->writeln('All test results were fine');
+	}
+
+	private function test_ping($output) {
+		$output->write(' - Pinging Solr: ');
+		if (!$this->solrAdmin->ping($ierror)) {
+			$output->writeln('<error>fail</error> - ' . $ierror->getCode());
+
+			return false;
+		}
+
+		$output->writeln('<info>ok</info>');
+
+		return true;
+	}
+
+	private function test_schema($output) {
+		$output->write(' - Checking Solr schema: ');
+		if (!$this->solrAdmin->checkSchema($ierror)) {
+			$output->writeln('<error>fail</error> - ' . $ierror->getCode());
+
+			return false;
+		}
+
+		$output->writeln('<info>ok</info>');
+
+		return true;
+	}
+
+	private function test_extract($output) {
+		$output->write(' - Extracting test document: ');
+		$doc = TestService::generateTestDocument(
+			1, '_nextant_test', __DIR__ . '/../../LICENSE', '/LICENSE'
+		);
+		$data = array(
+			$doc
+		);
+		$solrDocs = null;
+		$this->indexService->extract(
+			ItemDocument::TYPE_TEST, '_nextant_test', $data, $solrDocs, true, $ierror
+		);
+
+		if (!$doc->isProcessed()) {
+			$output->writeln('<error>fail</error> - ' . $ierror->getCode());
+
+			return false;
+		}
+
+		$output->writeln('<info>ok</info>');
+
+		return true;
+	}
+
+	private function test_update($output) {
+		$output->write(' - Updating test document: ');
+		$doc = TestService::generateTestDocument(
+			1, '_nextant_test', __DIR__ . '/../../LICENSE', '/LICENSE2'
+		);
+		$asource =
+			$this->indexService->getDocuments(ItemDocument::TYPE_TEST, '_nextant_test', 1, $ierror);
+
+		if ($asource === false || sizeof($asource) != 1 || (!key_exists('test_1', $asource))) {
+			$output->writeln('<error>fail</error> - ' . $ierror->getCode());
+
+			return false;
+		}
+
+		$source = $asource['test_1'];
+
+		$doc->setPath('/LICENSE2');
+		$doc->setShare(
+			array(
+				'nextant_test_share'
+			)
+		);
+		$doc->setShareGroup(
+			array(
+				'nextant_test_share_group'
+			)
+		);
+		$doc->deleted(false);
+
+		$data = array(
+			$doc
+		);
+		$this->indexService->updateDocuments(
+			ItemDocument::TYPE_TEST, '_nextant_test', $data, $asource, $ierror
+		);
+
+		if (!$this->solrTools->commit(false, $ierror)) {
+			$output->writeln('<error>fail</error> - ' . $ierror->getCode());
+
+			return false;
+		}
+
+		if (!$source->isUpdated()) {
+			$output->writeln('<error>fail</error> - ' . $ierror->getCode());
+
+			return false;
+		}
+
+		$output->writeln('<info>ok</info>');
+
+		return true;
+	}
+
+	private function test_search($output) {
+		$output->write(' - Searching test document: ');
+
+		$keyword = 'LICENSE';
+		$this->solrService->setOwner('_nextant_test');
+		if ($result = $this->solrService->search($keyword, array(), $ierror)) {
+			if (sizeof($result) > 0) {
+
+				foreach ($result as $doc) {
+					if ($doc->getType() === ItemDocument::TYPE_TEST && $doc->getId() === 1) {
+						$output->writeln('<info>ok</info>');
+
+						return true;
+					}
+				}
+
+				// CHECK ID DOCUMENT
+				$output->writeln('<error>fail</error>');
+
+				return false;
+			}
+
+			$output->writeln('<error>fail</error>');
+
+			return false;
+		}
+
+		$output->writeln('<error>fail</error> - ' . $ierror->getCode());
+
+		return false;
+	}
+
+	private function test_delete($output) {
+		$output->write(' - Deleting test document: ');
+
+		$doc = new ItemDocument(ItemDocument::TYPE_TEST, 1);
+		$data = array(
+			$doc
+		);
+		$this->indexService->removeDocuments($data, $ierror);
+		if ($doc->isRemoved()) {
+			$output->writeln('<info>ok</info>');
+
+			return true;
+		}
+
+		$output->writeln('<error>fail</error> - ' . $ierror->getCode());
+
+		return false;
+	}
 }
 
 
