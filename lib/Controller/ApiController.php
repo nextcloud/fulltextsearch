4467580cf6951356787bb1e7fe8ffc46e27356a6
Maxence Lange <maxence@artificial-owl.com>
2018-02-12 17:42:14 -0100

    cleaning
    Signed-off-by: Maxence Lange <maxence@artificial-owl.com>

--- lib/Controller/ApiController.php
+++ lib/Controller/ApiController.php
@@ -77,7 +77,6 @@ class ApiController extends Controller {
 	 * @return DataResponse
 	 */
 	public function search($request) {
-		$this->miscService->log('____' . json_encode($request));
 		return $this->searchDocuments(SearchRequest::fromJSON($request));
 	}
 
