aad1c9892e1e71b78af10983238786e250383880
Maxence Lange <maxence@artificial-owl.com>
2019-07-17 22:50:12 -0100

    fix
    Signed-off-by: Maxence Lange <maxence@artificial-owl.com>

--- lib/Command/DocumentIndex.php
+++ lib/Command/DocumentIndex.php
@@ -1,29 +1,4 @@
 <?php
-/**
- * FullTextSearch - Full text search framework for Nextcloud
- *
- * This file is licensed under the Affero General Public License version 3 or
- * later. See the COPYING file.
- *
- * @author Maxence Lange <maxence@artificial-owl.com>
- * @copyright 2018
- * @license GNU AGPL version 3 or any later version
- *
- * This program is free software: you can redistribute it and/or modify
- * it under the terms of the GNU Affero General Public License as
- * published by the Free Software Foundation, either version 3 of the
- * License, or (at your option) any later version.
- *
- * This program is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU Affero General Public License for more details.
- *
- * You should have received a copy of the GNU Affero General Public License
- * along with this program.  If not, see <http://www.gnu.org/licenses/>.
- *
- */
-
 declare(strict_types=1);
 
 
@@ -62,15 +37,13 @@ use OCA\FullTextSearch\Model\Index;
 use OCA\FullTextSearch\Service\MiscService;
 use OCA\FullTextSearch\Service\PlatformService;
 use OCA\FullTextSearch\Service\ProviderService;
-use OCP\FullTextSearch\Model\IIndexDocument;
 use Symfony\Component\Console\Input\InputArgument;
 use Symfony\Component\Console\Input\InputInterface;
-use Symfony\Component\Console\Input\InputOption;
 use Symfony\Component\Console\Output\OutputInterface;
 
 
 /**
- * Class DocumentProvider
+ * Class DocumentIndex
  *
  * @package OCA\FullTextSearch\Command
  */
@@ -88,7 +61,7 @@ class DocumentIndex extends Base {
 
 
 	/**
-	 * Index constructor.
+	 * DocumentIndex constructor.
 	 *
 	 * @param ProviderService $providerService
 	 * @param PlatformService $platformService
