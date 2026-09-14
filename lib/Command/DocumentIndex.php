6ff39dcaadc1424d39ff54b5cb5a34dd8a1a44f4
Ghassen kefi <ghassen.kefi.dev@gmail.com>
2026-09-14 13:08:32 +0200

    Fix: document index values in fulltextsearch OCC commands
    Signed-off-by: Ghassen kefi <ghassen.kefi.dev@gmail.com>

--- lib/Command/DocumentIndex.php
+++ lib/Command/DocumentIndex.php
@@ -11,9 +11,12 @@ namespace OCA\FullTextSearch\Command;
 
 use Exception;
 use OC\Core\Command\Base;
+use OCA\FullTextSearch\Exceptions\IndexDoesNotExistException;
 use OCA\FullTextSearch\Model\Index;
+use OCA\FullTextSearch\Service\IndexService;
 use OCA\FullTextSearch\Service\PlatformService;
 use OCA\FullTextSearch\Service\ProviderService;
+use Symfony\Component\Console\Input\InputOption;
 use Symfony\Component\Console\Input\InputArgument;
 use Symfony\Component\Console\Input\InputInterface;
 use Symfony\Component\Console\Output\OutputInterface;
@@ -22,6 +25,7 @@ class DocumentIndex extends Base {
 	public function __construct(
 		private ProviderService $providerService,
 		private PlatformService $platformService,
+		private IndexService $indexService,
 	) {
 		parent::__construct();
 	}
@@ -36,7 +40,8 @@ class DocumentIndex extends Base {
 			->setDescription('index one specific document')
 			->addArgument('userId', InputArgument::REQUIRED, 'userId')
 			->addArgument('providerId', InputArgument::REQUIRED, 'providerId')
-			->addArgument('documentId', InputArgument::REQUIRED, 'documentId');
+			->addArgument('documentId', InputArgument::REQUIRED, 'documentId')
+			->addOption('collection', null, InputOption::VALUE_OPTIONAL, 'collection to look the existing index up in, defaults to the internal collection');
 	}
 
 	/**
@@ -50,6 +55,7 @@ class DocumentIndex extends Base {
 		$providerId = $input->getArgument('providerId');
 		$documentId = $input->getArgument('documentId');
 		$userId = $input->getArgument('userId');
+		$collection = $input->getOption('collection');
 
 		$providerWrapper = $this->providerService->getProvider($providerId);
 		$provider = $providerWrapper->getProvider();
@@ -58,9 +64,9 @@ class DocumentIndex extends Base {
 		$index->setOwnerId($userId);
 		$index->setStatus(Index::INDEX_FULL);
 		try {
-			$index = \OC::$server->get(\OCA\FullTextSearch\Service\IndexService::class)->getIndex($providerId, $documentId);
-		} catch (\Throwable $t) {
-			$output->writeln("<error>Index not found : index attribute has been set to default values</error>");
+			$index = $this->indexService->getIndex($providerId, $documentId, $collection);
+		} catch (IndexDoesNotExistException $e) {
+			$output->writeln('<error>Index not found: index attributes have been set to default values</error>');
 		}
 		$indexDocument = $provider->updateDocument($index);
 		if (!$indexDocument->hasIndex()) {
